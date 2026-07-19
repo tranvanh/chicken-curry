7952f879dc0b0fb297d8b9ed9a9dae33a3ac5d44
tranvanh <tranvietanhcz@gmail.com>
2026-07-19 19:46:41 +0200

    test case

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use tensor::Tensor;
 
 fn main(){
     let tens = Tensor::new(
-        vec![3,2,2],
-        vec![1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0, 12.0]
+        vec![2,3,3],
+        vec![1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0, 12.0, 13.0,14.0,15.0,16.0,17.0,18.0],
     ).expect("Could not create tensor");
     println!("{}", tens);
-    let value = tens.get(&[2,1,1]).unwrap();
+    let value = tens.get(&[1,1,1]).unwrap();
 
-    println!("{}", value);
+    println!("at (1,1,1) = {}", value);
 }
